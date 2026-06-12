29e8dd0f8dc675a9af9e2193cfab61d32dfafab8
Alex Wood <alex.c.wood@gmail.com>
2026-06-12 13:47:21 -0400

    updated wrapping of event meta in show cards

--- src/components/molecules/EventMeta.tsx
+++ src/components/molecules/EventMeta.tsx
@@ -9,7 +9,7 @@ export function EventMeta({ icon, children }: EventMetaProps) {
   return (
     <p className="event-meta">
       <span aria-hidden="true">{icon}</span>
-      {children}
+      <span className="event-meta__content">{children}</span>
     </p>
   );
 }
